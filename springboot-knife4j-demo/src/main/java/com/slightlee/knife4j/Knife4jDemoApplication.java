536ab5eb992e3c59316117b92d506d4e18678309
morrow <limingwei@mentpeak.com>
2021-09-07 10:50:11 +0800

    feat: knife4j简单集成

---
+++ springboot-knife4j-demo/src/main/java/com/slightlee/knife4j/Knife4jDemoApplication.java
@@ -0,0 +1,13 @@
+package com.slightlee.knife4j;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Knife4jDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Knife4jDemoApplication.class, args);
+    }
+
+}
